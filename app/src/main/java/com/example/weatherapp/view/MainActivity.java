a453d3cf614e7b32f22d992f914f1e800f97c5a7
Rustam Galimov <Rustam.Galimov@icl-services.com>
2019-09-27 16:49:36 +0300

    Added material drawer dependency

--- app/src/main/java/com/example/weatherapp/view/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.java
@@ -5,16 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.weatherapp.R;
-import com.google.android.material.navigation.NavigationView;
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
-    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DrawerBuilder drawerBuilder = new DrawerBuilder(this);
+        drawerBuilder.withActivity(this).build();
     }
 }
